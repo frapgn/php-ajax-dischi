a37ce31fc0aab61fe24cfc7e45af7231fb2539e4
frapgn <59645305+frapgn@users.noreply.github.com>
2020-04-21 16:02:41 +0200

    solo php

--- data.php
+++ data.php
@@ -6,55 +6,55 @@
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1974',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/5/54/RastaRevolution_BobMarley.jpg'
-        ]
+        ],
         [
             'title'     => 'Natty Dread',
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1974',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/f/f4/BobMarley%26theWailersNattyDread.jpg'
-        ]
+        ],
         [
             'title'     => 'Rastaman Vibration',
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1976',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/6/66/BobMarley-RastamanVibration.jpg'
-        ]
+        ],
         [
             'title'     => 'Exodus',
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1977',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/b/b8/Bob_Marley_and_the_Wailers_-_Exodus.png'
-        ]
+        ],
         [
             'title'     => 'Kaya',
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1978',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/2/24/BobMarley-Kaya.jpg'
-        ]
+        ],
         [
             'title'     => 'Survival',
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1979',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/a/a3/Survival_Album.jpeg'
-        ]
+        ],
         [
             'title'     => 'Uprising',
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1979',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/0/0a/BobMarley%26theWailersUprising.jpg'
-        ]
+        ],
         [
             'title'     => 'Chances Are',
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1981',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/f/f8/ChancesAreAlbum.jpg'
-        ]
+        ],
         [
             'title'     => 'Confrontation',
             'artist'    => 'Bob Marley & The Wailers',
             'year'      => '1983',
             'cover'     => 'https://upload.wikimedia.org/wikipedia/en/b/bb/BobMarley-Confrontation.jpg'
-        ]
+        ],
         [
             'title'     => "Burnin'",
             'artist'    => 'The Wailers',
--- index.php
+++ index.php
@@ -5,20 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>php-ajax-dischi</title>
+    <link rel="stylesheet" href="dist/css/app.css">
+
 </head>
 <body>
 
     <header>
         <div id="logo">
-            <img src="" alt="">
+            <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn-ssl.funkidslive.com%2Fimg%2Fpodcast%2Fdeezer_w.png" alt="Logo">
         </div>
     </header>
 
     <main>
         <div class="container">
-            
+            <?php include 'data.php' ?>
+            <?php foreach ($albums as $value) { ?>
+                <div class="album">
+                    <div class="cover">
+                        <img src="<? echo $value['cover']?>" alt="">
+                    </div>
+                    <div class="title"><? echo $value['title']?></div>
+                    <div class="artist"><? echo $value['artist']?></div>
+                    <div class="year"><? echo $value['year']?></div>
+                </div>
+            <?php } ?>
+
         </div>
     </main>
 
+    <!-- Album Template -->
+    <script id="album-template" type="text/x-handlebars-template">
+        <div class="album">
+
+        </div>
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.6/handlebars.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <script src="dist/js/main.js" charset="utf-8"></script>
 </body>
 </html>
